ba11352a3177e4f11458cf5da2aef3eb628053fe
Manvitha <manvitha.vinay1@gmail.com>
2024-11-30 16:55:06 +0530

    Renamed js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import MenuList from "./components/MenuList";
 import Cart from "./components/Cart";
 import { CartProvider, useCart } from "./CartContext";
-import FeedbackForm from "./components/FeedbackForm";
+// import FeedbackForm from "./components/FeedbackForm";
 import "./index.css";
 
 const Popup = () => {
@@ -27,7 +27,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<MenuList />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/feedback-form" element={<FeedbackForm />} />
+            {/* <Route path="/feedback-form" element={<FeedbackForm />} /> */}
           </Routes>
         </div>
       </Router>
